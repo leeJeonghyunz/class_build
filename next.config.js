53c65bffbed45c0e73aa5e8b07c6827c668217c5
leeJeonghyunz <dlwjdus3217@naver.com>
2023-10-29 18:26:15 +0900

    분기배포

--- next.config.js
+++ next.config.js
@@ -2,6 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
+  generateBuildId: () => "class-build-deploy-project",
+
+  // 아래 주소들만 아웃폴더에 만들어줘!(getserversideprops제외!)
+  exportPathMap: () => {
+    return {
+      "/": { page: "/" },
+      "/boards": { page: "/boards" },
+      "/404": { page: "/404" },
+    };
+  },
 };
 
 module.exports = nextConfig;
--- pages/boards/[boardId]/index.js
+++ pages/boards/[boardId]/index.js
@@ -10,3 +10,8 @@ export default function BoardPage() {
     </>
   );
 }
+
+export const getServerSideProps = () => {
+  //만약 서버사이드 렌더링 페이지라면??=>out폴더 생성불가
+  //                                   =>이런 경우 next.config.js에서 exportpathMaps로
+};
